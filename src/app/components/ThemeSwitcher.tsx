e54fb6c8b7414694e963c5dd75cebe5a7ccadd95
Pedro Tozzi <pedrogbtozzi@gmail.com>
2023-08-08 21:42:56 -0300

    feat: add icon to ThemeSwitcher, fixing component export

--- src/app/components/ThemeSwitcher.tsx
+++ src/app/components/ThemeSwitcher.tsx
@@ -3,8 +3,9 @@
 import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
+import { Component1Icon, MoonIcon, SunIcon } from '@radix-ui/react-icons'
 
-export const ThemeSwitch = () => {
+export const ThemeSwitcher = () => {
     const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme()
 
@@ -23,9 +24,24 @@ export const ThemeSwitch = () => {
                 <SelectValue placeholder="Theme" />
             </SelectTrigger>
             <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
+                <SelectItem value="light">
+                    <div className='flex items-center'>
+                        <SunIcon className="w-4 h-4 mr-2" />
+                        Light
+                    </div>
+                </SelectItem>
+                <SelectItem value="dark">
+                    <div className='flex items-center'>
+                        <MoonIcon className="w-4 h-4 mr-2" />
+                        Dark
+                    </div>
+                </SelectItem>
+                <SelectItem value="system">
+                    <div className='flex items-center'>
+                        <Component1Icon className="w-4 h-4 mr-2" />
+                        System
+                    </div>
+                </SelectItem>
             </SelectContent>
         </Select>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 'use client'
 
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
 import { useTheme } from 'next-themes'
-import Image from 'next/image'
-import { ThemeSwitch } from './components/ThemeSwitcher'
+import { ThemeSwitcher } from './components/ThemeSwitcher'
 
 export default function Home() {
-  const { theme, setTheme } = useTheme()
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <ThemeSwitch />
+      <ThemeSwitcher />
     </div>
   )
 }
